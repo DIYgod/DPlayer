9a25245221a1684a4c58d48ca543c132a8f1dacb
DIYgod <diy.d.god@gmail.com>
2016-11-16 01:04:39 +0800

    getting bilibili danmaku and video by aid, close #37, close #34

--- nodejs/routes/video-bilibili.js
+++ nodejs/routes/video-bilibili.js
@@ -4,6 +4,20 @@ var redis = require('../tools/redis');
 var fetch = require('node-fetch');
 var md5 = require('blueimp-md5');
 
+var appkey = '4ebafd7c4951b366';
+var secret = '8cb98205e9b2ad3669aad0fce12a4c13';
+function getData(cid, res) {
+    var sign = md5(`appkey=${appkey}&cid=${cid}&otype=json&quality=2&type=mp4${secret}`);
+    fetch(`https://interface.bilibili.com/playurl?appkey=${appkey}&cid=${cid}&otype=json&quality=2&type=mp4&sign=${sign}`).then(
+        response => response.text()
+    ).then((data) => {
+            res.send(data);
+        }
+    ).catch(
+        e => logger.error("Bilibilib Error: getting data", e)
+    );
+}
+
 module.exports = function (req, res) {
     var ip = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
@@ -12,43 +26,31 @@ module.exports = function (req, res) {
 
     var query = url.parse(req.url,true).query;
     var aid = query.aid;
-    var appkey = '4ebafd7c4951b366';
-    var secret = '8cb98205e9b2ad3669aad0fce12a4c13';
+    var cid = query.cid;
 
-    redis.client.get(`bilibilicid${aid}`, function(err, reply) {
-        if (reply) {
-            logger.info(`Video Bilibili AV${aid} form redis, IP: ${ip}`);
+    if (cid) {
+        logger.info(`Bilibili cid2video ${cid}, IP: ${ip}`);
+        getData(cid, res);
+    }
+    else {
+        redis.client.get(`bilibiliaid2cid${aid}`, function(err, reply) {
+            if (reply) {
+                logger.info(`Bilibili aid2video ${aid} form redis, IP: ${ip}`);
+                getData(reply, res);
+            }
+            else {
+                logger.info(`Bilibili aid2video ${aid} form origin, IP: ${ip}`);
 
-            var sign = md5(`appkey=${appkey}&cid=${reply}&otype=json&quality=2&type=mp4${secret}`);
-            fetch(`https://interface.bilibili.com/playurl?appkey=${appkey}&cid=${reply}&otype=json&quality=2&type=mp4&sign=${sign}`).then(
-                response => response.text()
-            ).then((data) => {
-                    res.send(data);
-                }
-            ).catch(
-                e => logger.error("Video Bilibilib Error: getting data", e)
-            );
-        }
-        else {
-            logger.info(`Video Bilibili AV${aid} form origin, IP: ${ip}`);
-
-            fetch(`http://www.bilibili.com/widget/getPageList?aid=${aid}`).then(
-                response => response.json()
-            ).then((data) => {
-                    redis.set(`bilibilicid${aid}`, data[0].cid);
-                    var sign = md5(`appkey=${appkey}&cid=${data[0].cid}&otype=json&quality=2&type=mp4${secret}`);
-                    fetch(`https://interface.bilibili.com/playurl?appkey=${appkey}&cid=${data[0].cid}&otype=json&quality=2&type=mp4&sign=${sign}`).then(
-                        response => response.text()
-                    ).then((data) => {
-                            res.send(data);
-                        }
-                    ).catch(
-                        e => logger.error("Video Bilibilib Error: getting data", e)
-                    );
-                }
-            ).catch(
-                e => logger.error("Video Bilibilib Error: getting cid", e)
-            );
-        }
-    });
+                fetch(`http://www.bilibili.com/widget/getPageList?aid=${aid}`).then(
+                    response => response.json()
+                ).then((data) => {
+                        redis.set(`bilibiliaid2cid${aid}`, data[0].cid);
+                        getData(data[0].cid, res);
+                    }
+                ).catch(
+                    e => logger.error("Bilibili aid2video Error: getting cid", e)
+                );
+            }
+        });
+    }
 };
