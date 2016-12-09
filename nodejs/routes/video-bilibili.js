80ce0224539aac1b79554f792261e6f40ffcf4bb
DIYgod <diy.d.god@gmail.com>
2016-12-10 01:16:37 +0800

    add User-Agent

--- nodejs/routes/video-bilibili.js
+++ nodejs/routes/video-bilibili.js
@@ -16,7 +16,10 @@ function getData(cid, res, type) {
         res.send(api);
     }
     else {
-        fetch(api, { agent:new HttpProxyAgent('http://183.61.236.54:3128/')}).then(
+        fetch(api, {
+            headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'},
+            agent: new HttpProxyAgent('http://183.61.236.54:3128/')
+        }).then(
             response => response.text()
         ).then((data) => {
             parseString(data, {explicitArray: false}, function (err, result) {
