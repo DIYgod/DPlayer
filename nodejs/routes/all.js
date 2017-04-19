6edcac1e3ddc3705025c261735d6e45236de6205
DIYgod <diy.d.god@gmail.com>
2017-04-19 21:31:14 +0800

    use SimpleCount

--- nodejs/routes/all.js
+++ nodejs/routes/all.js
@@ -1,4 +1,8 @@
-module.exports = function(req, res, next) {
+var https = require('https');
+
+module.exports = function (req, res, next) {
+    https.get(`https://api.prprpr.me/count/?id=DIYgod-DPlayer&action=add`);
+    
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
