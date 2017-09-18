b8a16b509f8c99e0830d00f276059ab6e5ad5650
Donald Silveira <donald.silveira@lighthouseit.com.br>
2017-09-18 17:38:17 -0300

    feat: Added a option to set icons color

--- src/svg.js
+++ src/svg.js
@@ -3,18 +3,19 @@
 */
 
 class Svg {
-    constructor (icons) {
-        this.icons = icons;
+    constructor (options) {
+        this.icons = options.icons;
+        this.iconColor = options.iconColor;
     }
 
     get (type) {
         // Some SVG icons don't change icon size using viewBox. Ex.: Material Icons
-        // To fix these cases a optional index was added to icon object
+        // To fix these cases a optional index was added to icon object when change icon
+        // size is necessary
 
         return `
-            <svg xmlns:xlink="http://www.w3.org/1999/xlink" width="${this.icons[type][3] || '100%'}" height="${this.icons[type][3] || '100%'}" version="1.1" viewBox="${this.icons[type][0]}">
-                <use xlink:href="#dplayer-${type}"></use>
-                <path class="dplayer-fill" d="${this.icons[type][1]}" id="dplayer-${type}"></path>
+            <svg xmlns="http://www.w3.org/2000/svg" width="${this.icons[type][2] || '100%'}" height="${this.icons[type][2] || '100%'}" version="1.1" viewBox="${this.icons[type][0]}">
+                <path class="dplayer-fill" style="fill:${this.iconColor}" d="${this.icons[type][1]}" id="dplayer-${type}"></path>
             </svg>
         `;
     }
