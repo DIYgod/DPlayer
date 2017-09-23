a82f178d65fa9d109e9c4da1eae2fa67efc1d27e
DIYgod <diy.d.god@gmail.com>
2017-09-24 03:20:12 +0800

    subtitle events demo

--- demo.js
+++ demo.js
@@ -136,7 +136,8 @@ function initPlayers() {
         'quality_start', 'quality_end',
         'destroy',
         'resize',
-        'fullscreen', 'fullscreen_cancel', 'webfullscreen', 'webfullscreen_cancel'
+        'fullscreen', 'fullscreen_cancel', 'webfullscreen', 'webfullscreen_cancel',
+        'subtitle_show', 'subtitle_hide', 'subtitle_change'
     ];
     var eventsEle = document.getElementById('events');
     for (let i = 0; i < events.length; i++) {
