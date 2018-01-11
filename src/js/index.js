4395112b2203936ba132c611376a31600717bff5
DIYgod <diy.d.god@gmail.com>
2018-01-11 16:03:03 +0800

    rename DPlayer.js to player.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../css/index.scss';
-import DPlayer from './DPlayer';
+import DPlayer from './player';
 
 /* global DPLAYER_VERSION GIT_HASH */
 console.log(`${'\n'} %c DPlayer ${DPLAYER_VERSION} ${GIT_HASH} %c http://dplayer.js.org ${'\n'}${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
