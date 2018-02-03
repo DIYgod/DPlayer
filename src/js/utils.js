2eca9add4ba3505129548cbec6574ec7e5ae39d0
DIYgod <diy.d.god@gmail.com>
2018-02-03 14:11:19 +0800

    support hour in time string, @micooz and @Oukanina

--- src/js/utils.js
+++ src/js/utils.js
@@ -3,16 +3,17 @@ const isMobile = /mobile/i.test(window.navigator.userAgent);
 const utils = {
 
     /**
-    * Parse second to 00:00 format
+    * Parse second to time string
     *
     * @param {Number} second
-    * @return {String} 00:00 format
+    * @return {String} 00:00 or 00:00:00
     */
     secondToTime: (second) => {
         const add0 = (num) => num < 10 ? '0' + num : '' + num;
-        const min = parseInt(second / 60);
-        const sec = parseInt(second - min * 60);
-        return add0(min) + ':' + add0(sec);
+        const hour = Math.floor(second / 3600);
+        const min = Math.floor((second - hour * 3600) / 60);
+        const sec = Math.floor(second - hour * 3600 - min * 60);
+        return (hour > 0 ? [hour, min, sec] : [min, sec]).map(add0).join(':');
     },
 
     /**
