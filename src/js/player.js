87e137e2c3be7120407b3900c2e1b37c96e2b1b9
DIYgod <diy.d.god@gmail.com>
2018-06-14 00:00:19 +0800

    fix Hls is not supported in Safari Mobile

--- src/js/player.js
+++ src/js/player.js
@@ -333,6 +333,10 @@ class DPlayer {
                 }
             }
 
+            if (this.type === 'hls' && (video.canPlayType('application/x-mpegURL') || video.canPlayType('application/vnd.apple.mpegURL'))) {
+                this.type = 'normal';
+            }
+
             switch (this.type) {
             // https://github.com/video-dev/hls.js
             case 'hls':
