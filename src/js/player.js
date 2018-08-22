7889126c203ef89d8dbe2612af95e1fc9727e60a
A-Circle Zhang <cctvyay@163.com>
2018-08-22 12:22:19 +0800

    Update player.js

--- src/js/player.js
+++ src/js/player.js
@@ -436,6 +436,10 @@ class DPlayer {
 
         // video download error: an error occurs
         this.on('error', () => {
+            // Not a video load error, may be poster load failed, see #307
+            if (!this.video.error) {
+                return
+            }
             this.tran && this.notice && this.type !== 'webtorrent' & this.notice(this.tran('Video load failed'), -1);
         });
 
