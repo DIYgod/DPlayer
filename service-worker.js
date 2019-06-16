27416b8095763612b98e1081ed30aa3f6eecd009
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-16 11:38:45 +0000

    Deploy MoePlayer/DPlayer to github.com/MoePlayer/DPlayer.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,10 +21,10 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "358c4d535e3f01e3eabf44e1ab3d503f"
+    "revision": "4316865071d484946a958121742e31b5"
   },
   {
-    "url": "assets/css/0.styles.d2334894.css",
+    "url": "assets/css/0.styles.5677b0ce.css",
     "revision": "c1e6a2dcd690715ff1c74bf0f500848c"
   },
   {
@@ -32,23 +32,23 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.7e9ba447.js",
-    "revision": "6c817c125aa568c3e1de2fa871e8a940"
+    "url": "assets/js/10.9aa4cc77.js",
+    "revision": "2711bad4ecfe5f6b6d072cb6a07e147e"
   },
   {
-    "url": "assets/js/11.9599aa46.js",
-    "revision": "290037acdcc34e4afc750035e0793f9d"
+    "url": "assets/js/11.ccc6c15f.js",
+    "revision": "5c11bbd55f52b8d2295a5cf37e2a3919"
   },
   {
-    "url": "assets/js/12.9db55096.js",
-    "revision": "c1c249e745f7dc750be33eb428635c6a"
+    "url": "assets/js/12.a4e790ac.js",
+    "revision": "f1d30b4fa197287789c831064f9417b4"
   },
   {
     "url": "assets/js/13.a9da3c73.js",
     "revision": "b1d6f5726a2833ecb76351d4bf0e1b40"
   },
   {
-    "url": "assets/js/2.9e704d64.js",
+    "url": "assets/js/2.48e42532.js",
     "revision": "684d91cdc533ee735e1c1a174798d07b"
   },
   {
@@ -56,40 +56,40 @@ self.__precacheManifest = [
     "revision": "90c0e64f8bf6ac2589b5fa17a5c805c1"
   },
   {
-    "url": "assets/js/4.17430cde.js",
-    "revision": "96c4dfdbb2abaf921bd78e81aa5a7813"
+    "url": "assets/js/4.67ec63dd.js",
+    "revision": "48db692f73da3b892968eb5cf7447260"
   },
   {
-    "url": "assets/js/5.60a63446.js",
-    "revision": "924f3153ac2d78db1094d2fb375d27b3"
+    "url": "assets/js/5.707b2be2.js",
+    "revision": "26ec8929e73527e013ba9fc4ce223582"
   },
   {
-    "url": "assets/js/6.0a958f91.js",
-    "revision": "c0a821261de3805841b5f37b3ef98509"
+    "url": "assets/js/6.e871114b.js",
+    "revision": "ae5f57cfc237392966df5ed0d18f924d"
   },
   {
-    "url": "assets/js/7.070431b3.js",
-    "revision": "3bb44852edee7178c3afdf9de4319427"
+    "url": "assets/js/7.35bbd1fd.js",
+    "revision": "ab5f188baf73186a9e95e511987a31a6"
   },
   {
-    "url": "assets/js/8.292bc362.js",
-    "revision": "f8eaae474934816316772e76f3134ce7"
+    "url": "assets/js/8.76e605a5.js",
+    "revision": "bafd377b34cb6a0fafb205dbe7b49ba6"
   },
   {
-    "url": "assets/js/9.51ca288a.js",
-    "revision": "271a95538930c8f7915cb3ce059facf8"
+    "url": "assets/js/9.44d81a20.js",
+    "revision": "93479c47971eda359d7f900e3e08e225"
   },
   {
-    "url": "assets/js/app.5c3599c3.js",
-    "revision": "c60e32fccfa34aa33d3b534ad36772ab"
+    "url": "assets/js/app.ed4665ff.js",
+    "revision": "d1cf170cb107fb9a147a3900d6d6f7e2"
   },
   {
     "url": "ecosystem.html",
-    "revision": "74d8191845867f913a88f59ba1ab64a0"
+    "revision": "81b01a449dbf1edf765250de05ed6ea2"
   },
   {
     "url": "index.html",
-    "revision": "60a9b119908552be99ddf820438a426a"
+    "revision": "9613dbe42d6ea4f74e7803c66035c0ff"
   },
   {
     "url": "logo.png",
@@ -97,19 +97,19 @@ self.__precacheManifest = [
   },
   {
     "url": "support.html",
-    "revision": "870e37fbb62b82fb97a289bf101c775d"
+    "revision": "0cf39cf3639e0974b0c7bda62903e802"
   },
   {
     "url": "zh/ecosystem.html",
-    "revision": "26e29d09db4b922076dce087f107c699"
+    "revision": "5ceab26d1add5c7a7a1d4bcf2d7b68c3"
   },
   {
     "url": "zh/index.html",
-    "revision": "a9165d71d108380b714c8f27bc6b74f8"
+    "revision": "f4c4c89ee96897aef9c5164af5366d6c"
   },
   {
     "url": "zh/support.html",
-    "revision": "5e5b6bf7e5b0d1fafa9ec414249fe0fe"
+    "revision": "2926554f81150866914fcc7d875eeee5"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
