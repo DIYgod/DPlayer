7545dc0c40667b71cee25cccad80a6f8ad7b2383
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-10 09:28:35 +0000

    Deploy MoePlayer/DPlayer to github.com/MoePlayer/DPlayer.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "658e75ea4d8765c9629e5ef06059f298"
+    "revision": "7e6b22d05d71a59ab6c6a7cf1ce1b95d"
   },
   {
     "url": "assets/css/0.styles.5f20ea74.css",
@@ -38,23 +38,23 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.9ba5cb9b.js",
+    "url": "assets/js/10.2baf3d6b.js",
     "revision": "6d6b61efbcdb9893b336586bb3cfa06f"
   },
   {
-    "url": "assets/js/11.c355536e.js",
+    "url": "assets/js/11.45b13a4f.js",
     "revision": "41f87de2faa932b3f0d428b36565acfe"
   },
   {
-    "url": "assets/js/12.e023b0de.js",
+    "url": "assets/js/12.c488cb95.js",
     "revision": "27ca977f1f3c59ca0638216f697e0868"
   },
   {
-    "url": "assets/js/13.95e7d811.js",
+    "url": "assets/js/13.d4b567c4.js",
     "revision": "5c97f97c7d569ecbf658abfb99144bde"
   },
   {
-    "url": "assets/js/14.be5427b7.js",
+    "url": "assets/js/14.4786e05b.js",
     "revision": "dec3d62c2361dc5f96c2d54784b6f883"
   },
   {
@@ -62,7 +62,7 @@ self.__precacheManifest = [
     "revision": "e70f017166eb434af4dd23ecabf94888"
   },
   {
-    "url": "assets/js/2.b2709c0a.js",
+    "url": "assets/js/2.61da0266.js",
     "revision": "c83fbbb3068838a4dc8d1d83bad28253"
   },
   {
@@ -70,44 +70,44 @@ self.__precacheManifest = [
     "revision": "6fed6d57dcac609ec15f48e536ba31ed"
   },
   {
-    "url": "assets/js/4.f72dec27.js",
+    "url": "assets/js/4.cad65934.js",
     "revision": "60592ff03a1146d34dedfa3a426382de"
   },
   {
-    "url": "assets/js/5.7fd2d482.js",
+    "url": "assets/js/5.95b6bbdf.js",
     "revision": "007c48d307479b8d95ccecf78e9fbe67"
   },
   {
-    "url": "assets/js/6.62b62e05.js",
+    "url": "assets/js/6.133a82ce.js",
     "revision": "7040c032cc67e0f34e2dd1c8731df83e"
   },
   {
-    "url": "assets/js/7.b4d37333.js",
+    "url": "assets/js/7.c8c470cc.js",
     "revision": "3f59e056dd19f234bf71cbb4da865910"
   },
   {
-    "url": "assets/js/8.057a4c99.js",
+    "url": "assets/js/8.d03a3b1d.js",
     "revision": "5474568a78faa40259e13adfe25a4243"
   },
   {
-    "url": "assets/js/9.e0a46fd7.js",
+    "url": "assets/js/9.6d30e41e.js",
     "revision": "51342f409200508815e10c763f1c08ef"
   },
   {
-    "url": "assets/js/app.468b3852.js",
-    "revision": "76231892b7cb2a030eef5be87b8cc67f"
+    "url": "assets/js/app.4a90f17f.js",
+    "revision": "6dfc7999cb4e4282c517ed4862f105c7"
   },
   {
     "url": "ecosystem.html",
-    "revision": "a06b282202d9e33f1b818d0310f82c98"
+    "revision": "db54b82401a28afcf08a883c8e2b2b47"
   },
   {
     "url": "guide.html",
-    "revision": "d6bf9beac3e751b1777dafdab362d82c"
+    "revision": "c724ad73a4776b500aa5551d9aa0fd94"
   },
   {
     "url": "index.html",
-    "revision": "a4126d71d263011ece931920ee5d6e0f"
+    "revision": "71c4b02d5287100cd22164af6e2e1ff0"
   },
   {
     "url": "logo.png",
@@ -115,23 +115,23 @@ self.__precacheManifest = [
   },
   {
     "url": "support.html",
-    "revision": "77ccdc82326b6a28eef4f9d082220af7"
+    "revision": "21da7f2b1f1807f2a9eceb4993ebb284"
   },
   {
     "url": "zh/ecosystem.html",
-    "revision": "5173a13f14a9cbd3f66d4b9ca4d4679b"
+    "revision": "76fc9606fd99bd15a8d2f7ead1f61a3c"
   },
   {
     "url": "zh/guide.html",
-    "revision": "c74825f8e4cf5fa66939c3f1ed8e5639"
+    "revision": "029e8f30ff92be77f22a7c48485d574b"
   },
   {
     "url": "zh/index.html",
-    "revision": "2d0c2280da6cd7523746994b38c48451"
+    "revision": "1e100abc45dcc00a93f58744c984ed07"
   },
   {
     "url": "zh/support.html",
-    "revision": "dd87892a6bbb0f707ebd1a1e82930db5"
+    "revision": "1d8803533f22bf8daf562b94dde5fc47"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
