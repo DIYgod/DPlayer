44a3cd923dba0755f832b6b25af883c5002aee3a
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2020-07-23 21:51:03 +0300

    added chromecast support

--- src/js/options.js
+++ src/js/options.js
@@ -12,6 +12,7 @@ export default (options) => {
         lang: (navigator.language || navigator.browserLanguage).toLowerCase(),
         screenshot: false,
         airplay: true,
+        chromecast: false,
         hotkey: true,
         preload: 'metadata',
         volume: 0.7,
